d42cc44e286183419484f55234bca0fcdea8169a
髙野裕太郎 <ytaaroo@gmail.com>
2020-10-14 00:37:52 +0900

    scene.fxml 完成

--- src/main/java/cosmos/gui/FXMLController.java
+++ src/main/java/cosmos/gui/FXMLController.java
@@ -1,16 +1,10 @@
 package cosmos.gui;
 
 import cosmos.mariadb.ConnectDB;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Window;
-import javafx.util.Duration;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,24 +16,9 @@ public class FXMLController implements Initializable {
     private Label label;
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        label.setText("Hello, JavaFX\nRunning on Java.");
-
-        Timeline timer = new Timeline(
-                new KeyFrame(Duration.seconds(5), event -> {
-                    //現在表示されている画面を閉じる
-                    Scene scene1 = (label.getScene());
-                    Window window = scene1.getWindow();
-                    window.hide();
-
-                    //新しい画面を生成する
-                    try {
-                        new GenerateWindow("/signage.fxml","/styles.css","JavaFX and Gradle");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                })
-        );
-        timer.play();
+    public void initialize(URL url, ResourceBundle rb) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
 }
